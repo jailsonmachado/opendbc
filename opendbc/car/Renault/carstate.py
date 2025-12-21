1e9ab8dd901f642c6a0a37073d8dceab22da7c8e
Jailson Machado <jailsonsmachado@gmail.com>
2025-12-21 00:29:12 -0300

    Update carstate.py

--- opendbc/car/Renault/carstate.py
+++ opendbc/car/Renault/carstate.py
@@ -1,33 +1,57 @@
 from opendbc.can.parser import CANParser
-from car.renault.values import CAN, CAR
+from common.conversions import Conversions as CV
+from selfdrive.car.interfaces import CarStateBase
+from selfdrive.car.renault.values import CAN
 
-class CarState:
+class CarState(CarStateBase):
     def __init__(self, CP):
-        self.CP = CP
-        self.cruise_buttons = 0
-        self.is_metric = True
+        super().__init__(CP)
+        # Inicializa variáveis de estado se necessário
 
     def update(self, cp):
-        ret = car.CarState.new_message()
-        
-        # Exemplo: Velocidade das Rodas (ID 0x226)
-        # Os fatores (0.01) devem ser confirmados com o carro em movimento
-        ret.wheelSpeeds.fl = cp.vl[CAN.WHEEL_SPEEDS]['WHEEL_SPEED_FL'] * 0.01
-        ret.wheelSpeeds.fr = cp.vl[CAN.WHEEL_SPEEDS]['WHEEL_SPEED_FR'] * 0.01
+        ret = CarStateBase.new_message()
+
+        # 1. Velocidade do Veículo (vEgo)
+        # Usamos a média das rodas frontais (ID 0x226 no teu log)
+        # O fator 0.01 foi estimado; se a velocidade no ecrã do OP estiver errada, ajustamos aqui.
+        ret.wheelSpeeds.fl = cp.vl[CAN.WHEEL_SPEEDS]['WHEEL_SPEED_FL'] * 0.01 * CV.KPH_TO_MS
+        ret.wheelSpeeds.fr = cp.vl[CAN.WHEEL_SPEEDS]['WHEEL_SPEED_FR'] * 0.01 * CV.KPH_TO_MS
         ret.vEgo = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr) / 2.0
-        
-        # Estado do Volante (ID 0x1ab)
+        ret.vEgoRaw = ret.vEgo
+        ret.standstill = ret.vEgoRaw < 0.1
+
+        # 2. Direção (Ângulo e Torque)
+        # Retirado do ID 0x1ab (48 bytes) que vimos no log
         ret.steeringAngleDeg = cp.vl[CAN.ADAS_STATUS]['STEER_ANGLE']
-        ret.steeringPressed = abs(cp.vl[CAN.ADAS_STATUS]['STEER_TORQUE_SENSOR']) > 50
+        ret.steeringRateDeg = 0.0 # Pode ser calculado se necessário
+        
+        # Torque aplicado pelo condutor (para saber se as mãos estão no volante)
+        ret.steeringTorque = cp.vl[CAN.ADAS_STATUS]['STEER_TORQUE_SENSOR']
+        ret.steeringPressed = abs(ret.steeringTorque) > 50 # Limiar de detecção de mãos
+
+        # 3. Acelerador
+        # ID 0x303 (GAS_PEDAL)
+        ret.gas = cp.vl[CAN.GAS_PEDAL]['GAS_PEDAL_POS'] / 100.0
+        ret.gasPressed = ret.gas > 1e-5
+
+        # 4. Cruise Control e Travão (Stock)
+        # Nota: Estes IDs ainda precisam de ser confirmados quando tiveres o hardware,
+        # pois geralmente exigem que o Cruise Control esteja ligado para aparecerem.
+        ret.brakePressed = False # TODO: Mapear ID do travão
+        ret.cruiseState.enabled = True # Forçamos para testes, mas deve vir da rede CAN
+        ret.cruiseState.available = True
 
         return ret
 
     @staticmethod
     def get_can_parser(CP):
-        # Aqui definimos quais IDs o parser deve "escutar"
+        # Aqui definimos quais as mensagens que o openpilot deve "vigiar" na rede
         messages = [
-            (CAN.WHEEL_SPEEDS, 50),
+            # ID, Frequência (Hz)
             (CAN.ADAS_STATUS, 100),
+            (CAN.WHEEL_SPEEDS, 50),
             (CAN.GAS_PEDAL, 100),
         ]
-        return CANParser("renault_megane_etech", messages, 0) # 0 é o bus da câmera
+
+        # O nome "megane_etech" deve ser igual ao nome do teu ficheiro .dbc
+        return CANParser("megane_etech", messages, 0)
