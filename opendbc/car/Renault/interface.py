07458d53ffb2b7ed9516f645202036bccd9869e5
Jailson Machado <jailsonsmachado@gmail.com>
2025-12-21 00:46:15 -0300

    Update interface.py

--- opendbc/car/Renault/interface.py
+++ opendbc/car/Renault/interface.py
@@ -1,34 +1,40 @@
-from cereal import car
-from opendbc.car import get_safety_config
+from opendbc.car import structs, get_safety_config
 from opendbc.car.interfaces import CarInterfaceBase
+from opendbc.car.renault.carcontroller import CarController
+from opendbc.car.renault.carstate import CarState
 from opendbc.car.renault.values import CAR
 
+# Atalho para facilitar a leitura
+CarParams = structs.CarParams
+
 class CarInterface(CarInterfaceBase):
-    @staticmethod
-    def get_params(candidate, fingerprint=None, car_fw=None, experimental_long=False, docs=False):
-        ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
-        ret.carName = "renault"
-        
-        # Configuração de Segurança (Safety Model)
-        # Usamos Nissan como base de barramento para o Panda
-        ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.nissan)]
-        
-        # Parâmetros físicos específicos do Megane E-Tech
-        if candidate == CAR.MEGANE_ETECH:
-            ret.mass = 1708.        # kg
-            ret.wheelbase = 2.68    # metros
-            ret.steerRatio = 14.5   # Relação de direção
-            ret.centerToFront = ret.wheelbase * 0.44
-            
-        # Tempos de resposta do atuador
-        ret.steerActuatorDelay = 0.1
-        ret.steerLimitTimer = 0.4
-        
-        # Habilita o suporte obrigatório para as mensagens longas (48/64 bytes) do Megane
-        ret.flags |= car.CarParams.Flags.CANFD
+  CarState = CarState
+  CarController = CarController
 
-        return ret
+  @staticmethod
+  def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
+    ret.brand = 'renault'
 
-    # Método que faz a ponte com o CarController para enviar os comandos
-    def apply(self, c, now_nanos):
-        return self.CC.update(c, self.CS, now_nanos)
+    # Modelo de segurança. Inicialmente usamos Nissan para CAN-FD, 
+    # pois o suporte Renault CAN-FD no Panda é derivado dele.
+    ret.safetyConfigs = [get_safety_config(CarParams.SafetyModel.nissan)]
+
+    # Habilita o suporte a CAN-FD (Crucial para Megane E-Tech)
+    ret.flags |= CarParams.Flags.CANFD.value
+
+    # Configurações de Direção
+    ret.steerActuatorDelay = 0.1  # Megane responde rápido
+    ret.steerLimitTimer = 0.4
+    ret.steerAtStandstill = True
+
+    # Definimos como torque, pois o Megane (via câmera) recebe comandos de torque
+    ret.steerControlType = CarParams.SteerControlType.torque
+
+    # Dados físicos da plataforma CMF-EV (Megane)
+    if candidate == CAR.MEGANE_ETECH:
+      ret.mass = 1708.
+      ret.wheelbase = 2.68
+      ret.steerRatio = 14.5
+      ret.centerToFront = ret.wheelbase * 0.44
+
+    return ret
