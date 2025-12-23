e0aaec6a7793d22d3e0308e50d7c74a4c61b5b42
Jailson Machado <jailsonsmachado@gmail.com>
2025-12-22 23:16:19 -0300

    Update carstate.py

--- opendbc/car/Renault/carstate.py
+++ opendbc/car/Renault/carstate.py
@@ -1,7 +1,7 @@
 from opendbc.can.parser import CANParser
 from opendbc.car.interfaces import CarStateBase
-from opendbc.car.renault.values import CAR, CAN
-from common.conversions import Conversions as CV # Importante para m/s
+from opendbc.car.renault.values import CAR, DBC
+from common.conversions import Conversions as CV
 
 class CarState(CarStateBase):
   def __init__(self, CP):
@@ -10,52 +10,61 @@ class CarState(CarStateBase):
   def update(self, cp):
     ret = CarStateBase.new_message()
 
-    # --- 1. VELOCIDADE E RODAS (Mesclado com CV) ---
-    # Multiplicamos pelo fator do DBC (0.01) e convertemos para Metros por Segundo
-    ret.wheelSpeeds.fl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"] * 0.01 * CV.KPH_TO_MS
-    ret.wheelSpeeds.fr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FR"] * 0.01 * CV.KPH_TO_MS
-    ret.wheelSpeeds.rl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RL"] * 0.01 * CV.KPH_TO_MS
-    ret.wheelSpeeds.rr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RR"] * 0.01 * CV.KPH_TO_MS
+    # --- 1. VELOCIDADE E RODAS ---
+    # O DBC ja entrega em km/h (ex: 44.0). So precisamos converter para m/s.
+    # NAO multiplique por 0.01 aqui, pois o DBC ja fez isso!
+    ret.wheelSpeeds.fl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.fr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FR"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.rl = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RL"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.rr = cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RR"] * CV.KPH_TO_MS
     
-    # Média das 4 rodas em m/s
+    # Media das 4 rodas em m/s
     ret.vEgo = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.0
     ret.vEgoRaw = ret.vEgo
     
-    # Standstill: Usamos o bit do DBC se disponível, ou a velocidade < 0.1 m/s
+    # Standstill: velocidade < 0.1 m/s ou sinal da camera
     ret.standstill = cp.vl["ADAS_CAM_MISC"]["STANDSTILL"] == 1 or ret.vEgoRaw < 0.1
 
-    # --- 2. DIREÇÃO ---
+    # --- 2. DIRECAO ---
     ret.steeringAngleDeg = cp.vl["ADAS_STATUS"]["STEER_ANGLE"]
     ret.steeringRateDeg = cp.vl["ADAS_STATUS"]["STEER_RATE"]
     ret.steeringTorque = cp.vl["ADAS_STATUS"]["STEER_TORQUE_SENSOR"]
+    # Ajuste inicial: considera mao no volante se torque > 50 unidades (pode precisar ajuste)
     ret.steeringPressed = abs(ret.steeringTorque) > 50 
 
-    # --- 3. ACELERADOR E TRAVÃO (Sinais Reais do DBC) ---
+    # --- 3. ACELERADOR E FREIO ---
     ret.gas = cp.vl["GAS_PEDAL"]["GAS_PEDAL_POS"] / 100.0
     ret.gasPressed = ret.gas > 1e-5
-    ret.brakePressed = cp.vl["BRAKE_STATE"]["BRAKE_PRESSED"] == 1
+    
+    # Usando o CRUISE_STATE (ID 768) como backup, ja que o antigo BRAKE_STATE (311) nao existe
+    ret.brakePressed = cp.vl["CRUISE_STATE"]["BRAKE_PRESSED"] == 1
 
-    # --- 4. SEGURANÇA (Obrigatório para o OpenPilot) ---
-    ret.seatbeltUnlatched = cp.vl["CAR_STATE_SIGNALS"]["SEATBELT_DRIVER"] == 0
-    ret.doorOpen = cp.vl["CAR_STATE_SIGNALS"]["DOOR_OPEN_DRIVER"] == 1
+    # --- 4. SEGURANCA ---
+    # Como removemos o ID CAR_STATE_SIGNALS do DBC (nao existia no log),
+    # travamos esses valores como falsos para nao dar erro no startup.
+    ret.seatbeltUnlatched = False 
+    ret.doorOpen = False
 
-    # --- 5. CRUISE CONTROL (Mapeado do ADAS) ---
-    ret.cruiseState.enabled = cp.vl["ADAS_CAM_STATE"]["ACC_ACTIVE"] == 1
+    # --- 5. CRUISE CONTROL ---
+    # Logica 'OU': Se a Camera OU o painel (768) disserem que esta ativo, entao esta.
+    ret.cruiseState.enabled = cp.vl["ADAS_CAM_STATE"]["ACC_ACTIVE"] == 1 or cp.vl["CRUISE_STATE"]["CRUISE_ACTIVE"] == 1
     ret.cruiseState.available = True 
 
     return ret
 
   @staticmethod
   def get_can_parser(CP):
-    # Lista de mensagens que o parser deve ler do DBC
+    # IMPORTANTE: O nome aqui deve bater com o nome do arquivo na pasta dbc (sem a extensao)
+    # Baseado no seu link do GitHub: 'megane_etech.dbc' -> 'megane_etech'
+    dbc_name = "megane_etech"
+    
     messages = [
       ("ADAS_STATUS", 100),
       ("WHEEL_SPEEDS", 50),
       ("GAS_PEDAL", 50),
-      ("BRAKE_STATE", 50),
-      ("CAR_STATE_SIGNALS", 10),
       ("ADAS_CAM_STATE", 20),
       ("ADAS_CAM_MISC", 10),
+      ("CRUISE_STATE", 20), # ID 768
     ]
 
-    return CANParser("megane_etech", messages, 0)
+    return CANParser(dbc_name, messages, 0)
